8b0306adaaf4c3aa859dd60e1ebacd39228ee561
Joakim Carselind <j.carselind@gmail.com>
2012-03-06 12:38:25 +0100

    Added routes from ADD and configured them. Tested and they work

--- corpus/classify_handler.py
+++ corpus/classify_handler.py
@@ -6,6 +6,7 @@ import pymongo
 from classification_object import ClassificationObject
 
 class ClassifyHandler(tornado.web.RequestHandler):
+    # Example http://localhost:8888/classify?limit=10&skip=true leaves threshold as default
     def get(self):
         try:
             threshold = int(self.get_argument('threshold', 0.5))
@@ -18,18 +19,20 @@ class ClassifyHandler(tornado.web.RequestHandler):
             raise 'Intgers allowed'
 
         try:
-            skip = self.get_argument('skip', false)
+            skip = self.get_argument('skip', 'false')
         except ValueError:
             raise 'Valid values: {true, false}'
 
         # JSON list of matching classification objects
-        self.write()
+        self.write('Threshold %f, limit %d, skip %s' % (threshold, limit, skip))
 
     def post(self):
         try:
             c_obj = self.get_argument('classification_object')
         except TypeError:
-            raise ''
+            raise 'Classification objected allowed'
+
+        # perform classification on the given object
 
     @classmethod
     def _json_serializer(cls, obj):
@@ -38,19 +41,46 @@ class ClassifyHandler(tornado.web.RequestHandler):
         else:
             raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj))
 
-application = tornado.web.Application([
-    (r"/classify/(stream)?", ClassifyHandler),
-])
+class StreamHandler(tornado.web.RequestHandler):
+    def get(self):
+        try:
+            source_url = self.get_argument('source_url')
+        except TypeError:
+            raise 'Could not find source url'
 
-train = tornado.web.Application([
+        try:
+            threshold = self.get_argument('thresholds')
+        except TypeError:
+            raise ''
+
+class TrainHandler(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            source_url = self.get_argument('source_url')
+        except TypeError:
+            raise 'Could not find source url'
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('Test handler')
+
+class AlgorithmHandler(tornado.web.RequestHandler):
+    def get(self, algorithm_id):
+
+
+        self.write('Settings for algorithm %s is {} \n' % algorithm_id)
+
+    # Test with $curl -X PUT http://localhost:8888/algorithm/:id
+    def put(self, algorithm_id):
+
+        self.write('Updated settings for algorithm %s \n' % algorithm_id)
+
+
+application = tornado.web.Application([
+    (r"/classify", ClassifyHandler),
+    (r"/classify/stream", StreamHandler),
     (r"/train", TrainHandler),
-])
-
-test = tornado.web.Application([
     (r"/test", TestHandler),
-])
-
-application = tornado.web.Application([
     (r"/algorithm/([0-9]+)", AlgorithmHandler),
 ])
 
